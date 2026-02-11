661e4c8d55093aaa267bbdc4e542dbd947844953
eldofaaa <marcelloeldova@gmail.com>
2026-02-11 16:28:58 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap-icons/bootstrap-icons.min.css">
+</head>
+
+<body>
+    <!-- awal navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top shadow">
+        <div class="container">
+            <a href="" class="navbar-brand fw-bold"><i class="bi bi-anthropic"></i>SPIRATION</a>
+            <div class="collapse navbar-collapse">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item fw-bold"><a href="" class="nav-link">feature</a></li>
+                    <li class="nav-item fw-bold"><a href="" class="nav-link">about</a></li>
+                    <li class="nav-item fw-bold"><a href="" class="btn btn-success text-light">register</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- akhir navbar -->
+
+    <section class="hero text-center text-light bg-secondary min-vh-100 d-flex align-items-center pt-1 ">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-12">
+                    <img src="assets/img-removebg-preview.png" alt="" srcset="">
+                    <h1 class="display-4 fw-bold">student aspirations</h1>
+                    <p class="lead mt-3">website to convey aspirations about the school which will be submitted to the school</p>
+                </div>
+            </div>
+            <div class="row align-items-center">
+                <div class="col-lg-12">
+                    <a href="login" class="btn btn-light btn-lg mt-3 fw-medium">make your aspirations</a>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
